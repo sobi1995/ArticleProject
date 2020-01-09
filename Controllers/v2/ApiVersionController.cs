675ee2e7565d6e2e9dfd9968cfdb0dcf89c928dc
Baran <sobhan.plus.plus@gmail.com>
2020-01-09 23:59:42 +0330

    Swagger Fix Bug And Run

--- Controllers/v2/ApiVersionController.cs
+++ Controllers/v2/ApiVersionController.cs
@@ -4,9 +4,9 @@ namespace Versioning_ASP.Net_Core_3_APIs_with_Swashbuckle.Controllers.v2
     [ApiVersion("2")]
     [Route("api/v{version:apiVersion}/[controller]")]
      [ApiController]
-    public class ApiController:ControllerBase
+    public class ApiVersionController:ControllerBase
     {
             [HttpGet()]
-    public string Get() => "Space Potatoes v1";
+    public string Get() => "Space Potatoes v2";
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -30,50 +30,50 @@ namespace Versioning_ASP.Net_Core_3_APIs_with_Swashbuckle
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddApiVersioning(o =>
-           {
-               o.ReportApiVersions = true;
-               o.AssumeDefaultVersionWhenUnspecified = true;
-               o.DefaultApiVersion = new ApiVersion(1, 0);
-           });
+        
+            services.AddControllers();
+             services.AddApiVersioning(o =>
+            {
+                o.ReportApiVersions = true;
+                o.AssumeDefaultVersionWhenUnspecified = true;
+                o.DefaultApiVersion = new ApiVersion(1, 0);
+            });
+      
             services.AddSwaggerGen(option =>
-         {
-             option.SwaggerDoc("WithsaltWebApiDemo", new OpenApiInfo
-             {
-                 Version = "v1",
-                 Title = "WithsaltWebApiDemo API",
-                 Description = "API for WithsaltWebApiDemo",
-                 Contact = new OpenApiContact() { Name = "withsalt", Email = "withsalt@geeiot.net" }
-             });
+            {
+                option.SwaggerDoc("WithsaltWebApiDemo", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "WithsaltWebApiDemo API",
+                    Description = "API for WithsaltWebApiDemo",
+                    Contact = new OpenApiContact() { Name = "withsalt", Email = "withsalt@geeiot.net" }
+                });
 
-             option.SwaggerDoc("v1", new OpenApiInfo { Version = "v1", Title = "API V1" });
-             option.SwaggerDoc("v2", new OpenApiInfo { Version = "v2", Title = "API V2" });
+                option.SwaggerDoc("v1", new OpenApiInfo { Version = "v1", Title = "API V1" });
+                option.SwaggerDoc("v2", new OpenApiInfo { Version = "v2", Title = "API V2" });
 
-             option.DocInclusionPredicate((docName, apiDesc) =>
-             {
-                 var versions = apiDesc.CustomAttributes()
-                     .OfType<ApiVersionAttribute>()
-                     .SelectMany(attr => attr.Versions);
+                option.DocInclusionPredicate((docName, apiDesc) =>
+                {
+                    var versions = apiDesc.CustomAttributes()
+                        .OfType<ApiVersionAttribute>()
+                        .SelectMany(attr => attr.Versions);
 
-                 return versions.Any(v => $"v{v.ToString()}" == docName);
-             });
+                    return versions.Any(v => $"v{v.ToString()}" == docName);
+                });
 
-             option.OperationFilter<RemoveVersionParameterOperationFilter>();
-             option.DocumentFilter<SetVersionInPathDocumentFilter>();
+                option.OperationFilter<RemoveVersionParameterOperationFilter>();
+                option.DocumentFilter<SetVersionInPathDocumentFilter>();
 
                 // include document file
                 option.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, $"{typeof(Startup).Assembly.GetName().Name}.xml"), true);
-             option.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, $"Semicolon.Api.xml"), true);
-         });
+                option.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, $"Versioning-ASP.Net-Core-3-APIs-with-Swashbuckle.xml"), true);
+            });
 
             services.AddControllers()
                 .AddNewtonsoftJson(options =>
                 {
                     options.SerializerSettings.ContractResolver = new DefaultContractResolver();
                 });
-
-
-            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -95,14 +95,16 @@ namespace Versioning_ASP.Net_Core_3_APIs_with_Swashbuckle
                 endpoints.MapControllers();
             });
             app.UseStaticFiles();
+             app.UseSwagger();
+            //Enable middleware to serve swagger-ui (HTML, JS, CSS etc.), specifying the Swagger JSON endpoint
             app.UseSwaggerUI(option =>
-          {
-              option.SwaggerEndpoint("/swagger/v2/swagger.json", "V2 Docs");
-              option.SwaggerEndpoint("/swagger/v1/swagger.json", "V1 Docs");
+            {
+                option.SwaggerEndpoint("/swagger/v2/swagger.json", "V2 Docs");
+                option.SwaggerEndpoint("/swagger/v1/swagger.json", "V1 Docs");
 
-              option.RoutePrefix = string.Empty;
-              option.DocumentTitle = "WithsaltWebApiDemo API";
-          });
+                option.RoutePrefix = string.Empty;
+                option.DocumentTitle = "WithsaltWebApiDemo API";
+            });
         }
     }
 }
